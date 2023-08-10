6c977446863a6ea58ff98e73ca3a69d38fdafbe7
lleung030 <lucasleung2593@gmail.com>
2023-08-10 11:56:36 -0700

    chat screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,13 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack' 
 import React from 'react'
 import HomeScreen from './screens/HomeScreen';
+import ChatScreen from './screens/ChatScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
-        <Stack.screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Chat' component={ChatScreen} />
     </Stack.Navigator>
   )
 }
