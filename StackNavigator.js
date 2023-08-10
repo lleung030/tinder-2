505db2d3fa403f48913349f508b816a16a2e67c2
lleung030 <lucasleung2593@gmail.com>
2023-08-10 12:03:34 -0700

    login screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -2,14 +2,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import HomeScreen from './screens/HomeScreen';
 import ChatScreen from './screens/ChatScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+    const user = false;
   return (
-    <Stack.Navigator>
+      <Stack.Navigator>
+    {user ? (
+    <>
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='Chat' component={ChatScreen} />
+    </>
+    ) : (
+        <Stack.Screen name='Login' component={LoginScreen} />
+    )}
     </Stack.Navigator>
   )
 }
